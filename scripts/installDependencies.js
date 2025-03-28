449d20e9b7e13a3c7a5c52b96b7aee322f353145
j-ho <90638657+PJ-ho@users.noreply.github.com>
2025-03-28 16:19:11 +0800

    Update installDependencies.js
    fix mkdirSync is being used incorrectly

--- scripts/installDependencies.js
+++ scripts/installDependencies.js
@@ -197,9 +197,10 @@ if (platform == "macos") {
 
 	console.log("Setting up Swift UI monitoring...");
 	try {
-		const swiftSrc = mkdirSync(
-			path.join(binDir, "..", "temp", "ui_monitoring_macos.swift")
-		);
+		const tempDir = path.join(binDir, "..", "temp");
+		mkdirSync(tempDir, { recursive: true });
+
+		const swiftSrc = path.join(tempDir, "ui_monitoring_macos.swift");
 		await download(
 			`https://raw.githubusercontent.com/mediar-ai/screenpipe/refs/tags/v${process.env.SCREENPIPE_VERSION}/screenpipe-vision/src/ui_monitoring_macos.swift`,
 			swiftSrc
